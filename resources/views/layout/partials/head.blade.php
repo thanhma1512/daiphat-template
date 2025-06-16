3c4310db78ed8f280c814da48205d279aa697232
thanhma1512 <kienthanh265@gmail.com>
2025-06-16 16:12:46 +0700

    done

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -1,3 +1,2 @@
 @include('layout.partials.css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-ZAph/..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
